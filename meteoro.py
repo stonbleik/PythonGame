1a5eab90da0a3608d34da9acd26d6a0e369373c4
Antonio Sergio Loschi <141244873+stonbleik@users.noreply.github.com>
2025-05-18 10:19:28 -0300

    Add files via upload

---
+++ meteoro.py
@@ -0,0 +1,49 @@
+import pygame
+import math
+import random
+
+
+class Meteoro(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = pygame.image.load("data/meteor.png")
+        self.image = pygame.transform.scale(self.image, [100, 100])
+        self.rect = pygame.Rect(50, 50, 100, 100)
+
+        self.rect.x = 840 + random.randint(1, 400)
+        self.rect.y = random.randint(1, 400)
+
+        self.speed = 1 + random.random() * 2
+
+    def update(self, *args):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            # Enviar evento de game over
+            pygame.event.post(pygame.event.Event(
+                pygame.USEREVENT, {"gameover": True}))
+            self.kill()
+
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = pygame.image.load("data/alien.gif")
+        self.image = pygame.transform.scale(self.image, [100, 100])
+        self.rect = pygame.Rect(50, 50, 100, 100)
+
+        self.rect.x = 840 + random.randint(1, 400)
+        self.rect.y = random.randint(1, 400)
+
+        self.speed = 1 + random.random() * 2
+
+    def update(self, *args):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            # Enviar evento de game over
+            pygame.event.post(pygame.event.Event(
+                pygame.USEREVENT, {"gameover": True}))
+            self.kill()
